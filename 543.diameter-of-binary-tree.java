3d70c16391aa762df319d32b814c870db2054bec
Pawan Thapa <sajan.thapa12345@gmail.com>
2020-04-16 19:42:23 +0530

    Problem 543 Diameter of a binary tree

--- 543.diameter-of-binary-tree.java
+++ 543.diameter-of-binary-tree.java
@@ -11,30 +11,38 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-
+        if (root == null)
+            return 0;
+        return helper(root).maxD;
     }
 
-    public int diameterOfBinaryTree(TreeNode root){
-        if(root ==null){
-            diameter bc= new diameter();
-            bc.lh=0;
-            bc.rh=0;
-            bc.maxD=0;
+    public diameter helper(TreeNode root) {
+        if (root == null) {
+            diameter bc = new diameter();
+            bc.lh = 0;
+            bc.rh = 0;
+            bc.maxD = 0;
+            return bc;
         }
-        diameter l = diameterOfBinaryTree(root.left);
-        diameter r = diameterOfBinaryTree(root.right);
-        
+        diameter l = helper(root.left);
+        diameter r = helper(root.right);
+
         diameter res = new diameter();
-        res.lh= Math.max(l.lh, l.rh) +1;
-        res.rh= Math.max(r.lh, r.rh) +1;
-        res.maxD= Math.max(Math.max(l.maxD, r.maxD), );
-        
+        res.lh = Math.max(l.lh, l.rh);
+        res.rh = Math.max(r.lh, r.rh);
+        res.maxD = Math.max(Math.max(l.maxD, r.maxD), res.lh + res.rh);
+
+        res.lh += 1;
+        res.rh += 1;
+        return res;
+
     }
 
-    private class diameter(){
+    private class diameter {
         int lh;
         int rh;
         int maxD;
     }
+
 }
 // @lc code=end
